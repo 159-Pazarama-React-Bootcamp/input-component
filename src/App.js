3b527ebd49f447acec146745c484be3c0bd79a48
Ahmet Fatih Yavasi <afatihyavasi@gmail.com>
2022-01-03 21:26:44 +0300

    Basic input component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Input from "./components/Input";
 
 function App() {
+  const [form, setForm] = useState({ userName: "", password: "" });
+
+  const handleChange = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{ width: "400px" }}>
+      <Input
+        type={"text"}
+        name='userName'
+        value={form.userName}
+        onChange={handleChange}
+      />
+      <Input
+        type={"password"}
+        name='password'
+        value={form.password}
+        onChange={handleChange}
+      />
+      <div>{JSON.stringify(form)}</div>
     </div>
   );
 }
